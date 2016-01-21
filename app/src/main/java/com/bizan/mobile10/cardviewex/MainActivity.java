fcb52f5151d9d33b8b42bfc417e375dda128d9db
Dan_Imai <oinarhythm0508@gmail.com>
2016-01-21 14:23:16 +0900

    Don't touch me

--- app/src/main/java/com/bizan/mobile10/cardviewex/MainActivity.java
+++ app/src/main/java/com/bizan/mobile10/cardviewex/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    Snackbar.make(view, "Don't touch me!!", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
             });
